9a4b95054ee3a13ef6232b4c3d6648c6187cad9d
jinwoo45 <jinwoo3158@gmail.com>
2022-09-14 10:50:17 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login';
+import Member from './pages/Member';
+import Community from './pages/Community';
+import { Routes, Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Club from './pages/Club';
+import Admin from './pages/Admin';
+import Payment from './pages/Payment';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+          
+      <Routes>
+          <Route element={<Navbar/>}>
+          <Route path="/" element={<Member/>} />
+          <Route path="/member" element={<Member />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/club" element={<Club />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/nft" element={<Member />} />
+          <Route path="/question" element={<Member />} />
+          </Route>
+       
+          {/* 내비바 숨기고 싶은 페이지 */}
+          <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
