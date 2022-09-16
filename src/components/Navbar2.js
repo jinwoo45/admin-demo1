651a1d98d6536681312f0cdb47221ae2d83098b2
jinwoo45 <jinwoo3158@gmail.com>
2022-09-16 15:26:31 +0900

    search box

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -12,7 +12,7 @@ const Navbar2 = () => {
     <div className='nav2-right'>
       <div>admin님</div>
       <div>개인정보 수정</div>
-      <div><Link to= "/login">로그out</Link></div>
+      <div><Link to= "/login">로그아웃</Link></div>
   </div>
 </div>
   )
