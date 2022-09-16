fc8b187ed0488bbec5230a7e00c95737a12cb1cb
jinwoo45 <jinwoo3158@gmail.com>
2022-09-16 20:01:32 +0900

    member-search update

--- src/components/MemberSearch.js
+++ src/components/MemberSearch.js
@@ -4,7 +4,41 @@ const MemberSearch = () => {
   return (
     <div>
       <div className='search-box'>
-        <input></input>        
+        <form >
+      <div className='search-box-out'>    
+        <div className='search-box-in'>
+          <label>학교 : </label>
+            <select>
+              <option>중앙대학교</option>
+              <option>건국대학교</option>
+              <option>배제대학교</option>
+            </select>
+        </div>
+
+        <div className='search-box-in'>
+          <label>검색어 : </label>
+       
+       <div>
+            <select>
+              <option>회원번호</option>
+              <option>이름</option>
+              <option>닉네임</option>
+              <option>핸드폰번호</option>
+              <option>학과</option>
+            </select>
+            <input></input>
+          </div>
+        </div>
+
+        <div className='search-box-in'>
+          <label>가입일 : </label>
+          <input></input> ~ <input></input>
+        </div>
+      </div>
+        <div className='button-box'>
+         <button>검색</button>        
+        </div>
+        </form>
       </div>
     </div>
   )
