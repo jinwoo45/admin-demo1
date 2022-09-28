389933a6578832a593ed78f8997cb413689fa92a
tnmeta-ysr <ysr@tnmeta.io>
2022-09-28 15:16:24 +0900

    검색 화면 수정

--- src/pages/admin/SearchAdmin.js
+++ src/pages/admin/SearchAdmin.js
@@ -1,22 +1,30 @@
 import React from "react";
 import "./css/SearchAdmin.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchAdmin = () => {
     return (
         <div className="search-admin-container">
-            <select>
-                <option value="/">전체</option>
-                <option value="adminId">관리자ID</option>
-                <option value="adminGroup">그룹명</option>
-                <option value="adminDept">부서명</option>
-            </select>
-            <div className="search-input-wrap">
+            <div className="search-group-wrap">
+                <label>그룹명</label>
+                <select>
+                    <option>전체</option>
+                    <option>슈퍼관리자</option>
+                    <option>티앤메타관리자</option>
+                    <option>학교관리자</option>
+                </select>
+            </div>
+            <div>
+                <label>검색어</label>
+                <select>
+                    <option value="/">전체</option>
+                    <option value="adminId">관리자ID</option>
+                    <option value="adminDept">부서명</option>
+                </select>
                 <input type="text" />
-                <button className="search-admin-btn">
-                    <FontAwesomeIcon icon={faSearch} />
-                </button>
+            </div>
+            
+            <div className="search-btn-wrap">
+                <button type="button" className="search-admin-btn">검색</button>
             </div>
         </div>
     );
